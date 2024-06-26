c4f9e13329b5b5e0d871226be769c58ffe8ac88e
JoseCorreaMorales <jose.correax@gmail.com>
2024-06-25 22:23:09 -0600

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -106,6 +106,10 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+
+          <h2>Issue branch</h2>
+
+
         </a>
       </div>
     </main>
